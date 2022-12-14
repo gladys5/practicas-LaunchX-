e959a12b86c7325eeb47e8e515d02a8450be1038
gladys5 <yumanji@live.com>
2022-12-14 01:44:08 -0700

    a-vacunacion

--- vacunacion/script.js
+++ vacunacion/script.js
@@ -1,14 +1,3 @@
-// navar mostrar y cultar menu
-// addEventListener("DOMContentLoaded", function () {
-//   const btn_menu = document.querySelector("btn_menu")
-//   if (btn_menu) {
-//     btn_menu.addEventListener("click", () => {
-//       const menu_items = document.queryselector("menu_items")
-//       menu_items.classList.toggle("show")
-//     })
-//   }
-// })
-// // navar mostrar y cultar menu mas limpio
 function Showmenu() {
   document.getElementById("menu_items").classList.toggle("show")
 }
